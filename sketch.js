4c8f23ce853d683445c38f139475d81861c04463
Ahad0712 <85945961+Ahad0712@users.noreply.github.com>
2021-06-29 17:23:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,12 +8,13 @@ function setup() {
   speed=random(55,90);
   weight=random(400,1500);
 
+  car=createSprite(50,200,50,50);
+  wall=createSprite(1500,200,60,height/2);
 }
 
 function draw() {
   background(255,255,255);  
-  car=createSprite(50,200,50,50);
-  wall=createSprite(1500,200,60,height/2);
+
   car.velocityX= speed;
 
   if(wall.x-car.x < (car.width+wall.width)/2){
